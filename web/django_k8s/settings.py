fe3e90ed768f9ddfd035db44cf781102c02d8016
henryantwi <henryantwi191@gmail.com>
2025-03-10 13:11:55 +0000

    settings.py updated

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -38,7 +38,9 @@ ROOT_URLCONF = 'django_k8s.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
